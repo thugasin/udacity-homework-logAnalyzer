c3d4c499ba25cdaeb217fdadc227fcce9b762931
Feng Li <FLi4@slb.com>
2018-07-20 11:06:54 -0500

    support report all and some refactors

--- README.md
+++ README.md
@@ -1,14 +1,60 @@
 # logAnalyzer
+This is a reporting command  line tool implemented by python script using psycopg2 module to connect to database.</br>
+The database contains 3 tables,  newspaper articles, authors as well as the web server log for some site. The log has a database row for each time a reader loaded a web page.</br>
 
-copy logAnalyzer.py and logAnalyzerDb.py to the vagrant folder of virtual machine;
+The reporting tool provide 3 commands to support 3 kind of query :
+
+###1. What are the most popular three articles of all time? Which articles have been accessed the most? Present this information as a sorted list with the most popular article at the top.
+
+####Example:
+
+"Princess Shellfish Marries Prince Handsome" — 1201 views
+"Baltimore Ravens Defeat Rhode Island Shoggoths" — 915 views
+"Political Scandal Ends In Political Scandal" — 553 views
+###2. Who are the most popular article authors of all time? That is, when you sum up all of the articles each author has written, which authors get the most page views? Present this as a sorted list with the most popular author at the top.
+
+####Example:
+
+Ursula La Multa — 2304 views
+Rudolf von Treppenwitz — 1985 views
+Markoff Chaney — 1723 views
+Anonymous Contributor — 1023 views
+###3. On which days did more than 1% of requests lead to errors? The log table includes a column status that indicates the HTTP status code that the news site sent to the user's browser. (Refer to this lesson for more information about the idea of HTTP status codes.)
+
+####Example:
+
+July 29, 2016 — 2.5% errors
+
+## Requirements
+- [Vagrant](https://www.vagrantup.com/)
+- [VirtualBox](https://www.virtualbox.org/)
+
+## Build the application
+1. Clone the repo in  [provided by Udacity](https://github.com/udacity/fullstack-nanodegree-vm/blob/master/vagrant/Vagrantfile)
+
+2. Launch the command line console, type command<br> 
+```vagrant up```
+
+3. After the virtual machine is ready tpye command:<br>
+```vagrant ssh```
+
+4. copy logAnalyzer.py and logAnalyzerDb.py to the vagrant folder;
+5. copy the database file from: https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+6. back to the command line console, import the database by:<br>
+   ```psql -d news -f newsdata.sql```
+
+## Command lines for query
 ### get top popular articles record
 ``` python logAnalyzer.py toparticle [number]```
-   <br>the [number] is number of top articles to be retrieved
+   <br>the [number] (by default is 3) is number of top articles to be retrieved
 ### get top popular author record
 ``` python logAnalyzer.py toparticle [number]```
-   <br>the [number] is number of top articles to be retrieved
+   <br>the [number] (by default is 3) is number of top authors to be retrieved
 
 ### get the day of accident
 ``` python logAnalyzer.py accidentdays```
+
+### report all of the information above
+``` python logAnalyzer.py reportall```
 
 
--- logAnalyzer.py
+++ logAnalyzer.py
@@ -4,29 +4,59 @@ import logAnalyzerDb
 
 
 def main():
-    if (sys.argv[1].lower() == "toparticle"):
-        try:
-            results = logAnalyzerDb.get_top_popular(int(sys.argv[2]))
-            for result in results:
-                print('"{}" -- {} views'.format(result[0],result[1]))
-        except (Exception) as error:
-            print(error)
-    elif (sys.argv[1].lower() == "topauthor"):
-        try:
-            results = logAnalyzerDb.get_top_author(int(sys.argv[2]))
-            for result in results:
-                print('{} -- {} views'.format(result[0],result[1]))
-        except (Exception) as error:
-            print(error)
-    elif (sys.argv[1].lower() == "accidentdays"):
-        try:
-            results = logAnalyzerDb.get_show_stoper_days()
-            for result in results:
-                print('{} -- {:.1f}% errors'.format(result[0],result[1]))
-        except (Exception) as error:
-            print(error)
-    else:
-        print(sys.argv[1]+' command not found.')
+    try:
+        if (len(sys.argv)<2):
+            print("please input the query command!")
+            return
+        if (len(sys.argv)>2):
+            arg = int(sys.argv[2])
+        else:
+            arg = 3
+
+        if (sys.argv[1].lower() == "toparticle"):
+            report_top_articles(arg)
+        elif (sys.argv[1].lower() == "topauthor"):
+            report_top_authors(arg)
+        elif (sys.argv[1].lower() == "accidentdays"):
+            report_accident_days()
+        elif (sys.argv[1].lower() == "reportall"):
+            print("********************Top popular articles*****************************")
+            report_top_articles(arg)
+            print("********************Top popular authors*****************************")
+            report_top_authors(arg)
+            print("********************accident days*****************************")
+            report_accident_days()
+        else:
+            print(sys.argv[1]+' command not found.')
+    except (Exception) as error:
+        print(error)
+
+def report_accident_days():
+    """On which days did more than 1% of requests lead to errors"""
+    try:
+        results = logAnalyzerDb.get_show_stoper_days()
+        for result in results:
+            print('{} -- {:.1f}% errors'.format(result[0],result[1]))
+    except (Exception) as error:
+        print(error)
+
+def report_top_authors(num):
+    """Who are the most popular article authors of all time?"""
+    try:
+        results = logAnalyzerDb.get_top_author(num)
+        for result in results:
+            print('{} -- {} views'.format(result[0],result[1]))
+    except (Exception) as error:
+        print(error)
+
+def report_top_articles(num):
+    """On which days did more than 1% of requests lead to errors?"""
+    try:
+        results = logAnalyzerDb.get_top_popular(num)
+        for result in results:
+            print('"{}" -- {} views'.format(result[0],result[1]))
+    except (Exception) as error:
+        print(error)
 
 if __name__ == "__main__":
     main()
