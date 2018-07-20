1c71299663d86444dcc41ef83802f8e2deba8655
Feng Li <FLi4@slb.com>
2018-07-20 01:26:12 -0500

    refactor

--- logAnalyzerDb.py
+++ logAnalyzerDb.py
@@ -1,42 +1,49 @@
+#!/usr/bin/env python3
 import psycopg2
 
 DBNAME = "news"
 
-
-def get_top_popular(top_num):
+def execute_query(cmd):
+    """ common function for psql query """
     conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("""SELECT articles.title, COUNT(log.path) AS num
+    cursor.execute(cmd)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+def get_top_popular(top_num):
+    """ query the top(top_num) popular articles 
+        top_num => list of [title, count]
+    """
+    cmd = """SELECT articles.title, COUNT(log.path) AS num
                    FROM articles LEFT JOIN log 
                    ON log.path = '/article/'||articles.slug
-                   GROUP BY articles.title ORDER BY num DESC""")
-    results = cursor.fetchmany(top_num)
-    conn.commit()
-    conn.close()
-    return results
+                   GROUP BY articles.title ORDER BY num DESC LIMIT {}""".format(top_num)
+    return execute_query(cmd)
 
 
 def get_top_author(top_num):
-    conn = psycopg2.connect(database=DBNAME)
-    cursor = conn.cursor()
-    cursor.execute("""SELECT authors.name,author_result.num 
+    """ query the top(top_num) popular author 
+        top_num => list of [author, count]
+    """
+    cmd = """SELECT authors.name,author_result.num 
                     FROM authors JOIN 
                     (SELECT SUM(article_result.num) as num, article_result.author
                     from (SELECT articles.title, articles.author, COUNT(log.path) AS num 
                     FROM articles LEFT JOIN log ON log.path = '/article/'
                     || articles.slug GROUP BY articles.title, articles.author) AS article_result
                     GROUP BY article_result.author) as author_result
-                    ON authors.id = author_result.author ORDER BY num DESC""")
-    author_result = cursor.fetchmany(top_num)
-    conn.commit()
-    conn.close()
-    return author_result
+                    ON authors.id = author_result.author ORDER BY num DESC LIMIT {}""".format(top_num)
+    return execute_query(cmd)
 
 
 def get_show_stoper_days():
-    conn = psycopg2.connect(database=DBNAME)
-    cursor = conn.cursor()
-    cursor.execute("""SELECT time,error_report.bad*100/error_report.all::float as errors from (SELECT all_result.time::DATE,
+    """ query the accident(errors happened more than 1%) days 
+        => list of [date, error rate]
+    """
+    cmd = """SELECT time,error_report.bad*100/error_report.all::float as errors from (SELECT all_result.time::DATE,
              all_result.all, bad_result.bad FROM (SELECT
              time::DATE, COUNT(
              time::DATE) as all FROM log GROUP BY time::DATE)
@@ -46,8 +53,5 @@ def get_show_stoper_days():
              TIME::DATE) as bad_result ON
              all_result.time::DATE = bad_result.time::DATE)
              AS error_report WHERE
-             error_report.bad*100 > error_report.all""")
-    show_stoper_day = cursor.fetchall()
-    conn.commit()
-    conn.close()
-    return show_stoper_day
+             error_report.bad*100 > error_report.all"""
+    return execute_query(cmd)
